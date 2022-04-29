bbc76985af614faebdeec963722e100c02670899
congqian <congqian@yuxisoft.cn>
2022-04-29 09:57:11 +0800

    ff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './', // 防止打包之后资源引用404
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '_c', replacement: resolve(__dirname, 'src/components') }
+    ]
+  }
 })
